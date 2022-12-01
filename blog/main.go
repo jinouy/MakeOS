7ad7f08eea9cdf9be58723c578d2e34c37ffc068
jinouyang <joy@howlink.cn>
2022-12-01 11:29:20 +0800

    前缀树完善路由配置

--- blog/main.go
+++ blog/main.go
@@ -9,8 +9,26 @@ func main() {
 
 	engine := msgo.New()
 	g := engine.Group("user")
-	g.Get("/hello", func(ctx *msgo.Context) {
-		fmt.Fprintln(ctx.W, "hello joy.com")
+	g.Get("/hello/*/get", func(ctx *msgo.Context) {
+		fmt.Fprintln(ctx.W, " get hello joy.com")
+	})
+	g.Post("/hello", func(ctx *msgo.Context) {
+		fmt.Fprintln(ctx.W, "post hello joy.com")
+	})
+	g.Put("/hello", func(ctx *msgo.Context) {
+		fmt.Fprintln(ctx.W, "put hello joy.com")
+	})
+	g.Delete("/hello", func(ctx *msgo.Context) {
+		fmt.Fprintln(ctx.W, "delete hello joy.com")
+	})
+	g.Post("/info", func(ctx *msgo.Context) {
+		fmt.Fprintln(ctx.W, "post info joy.com")
+	})
+	g.Any("/any", func(ctx *msgo.Context) {
+		fmt.Fprintln(ctx.W, "any joy.com")
+	})
+	g.Get("/get/:id", func(ctx *msgo.Context) {
+		fmt.Fprintln(ctx.W, "get id joy.com")
 	})
 	engine.Run()
 
--- msgo/ms.go
+++ msgo/ms.go
@@ -8,67 +8,74 @@ import (
 
 const ANY = "ANY"
 
-type HandleFunc func(ctx *Context)
+type HandlerFunc func(ctx *Context)
 
 type router struct {
-	groups []*routerGroup
+	routerGroups []*routerGroup
 }
 
 func (r *router) Group(name string) *routerGroup {
 	g := &routerGroup{
 		groupName:        name,
-		handlerMap:       make(map[string]map[string]HandleFunc),
+		handlerMap:       make(map[string]map[string]HandlerFunc),
 		handlerMethodMap: make(map[string][]string),
+		treeNode:         &treeNode{name: "/", children: make([]*treeNode, 0)},
 	}
-	r.groups = append(r.groups, g)
+	r.routerGroups = append(r.routerGroups, g)
 	return g
 }
 
-func (r *routerGroup) handle(name string, method string, handleFunc HandleFunc) {
+func (r *routerGroup) handle(name string, method string, handlerFunc HandlerFunc) {
 	_, ok := r.handlerMap[name]
 	if !ok {
-		r.handlerMap[name] = make(map[string]HandleFunc)
+		r.handlerMap[name] = make(map[string]HandlerFunc)
 	}
-	r.handlerMap[name][method] = handleFunc
+	_, ok = r.handlerMap[method]
+	if ok {
+		panic("有重复的路由")
+	}
+	r.handlerMap[name][method] = handlerFunc
 	r.handlerMethodMap[method] = append(r.handlerMethodMap[method], name)
+	r.treeNode.Put(name)
 }
 
-func (r *routerGroup) Any(name string, handleFunc HandleFunc) {
+func (r *routerGroup) Any(name string, handlerFunc HandlerFunc) {
 
-	r.handle(name, ANY, handleFunc)
-}
-
-func (r *routerGroup) Handle(name string, method string, handleFunc HandleFunc) {
-
-	r.handle(name, method, handleFunc)
+	r.handle(name, ANY, handlerFunc)
 }
 
-func (r *routerGroup) Get(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodGet, handleFunc)
+func (r *routerGroup) Handle(name string, method string, handlerFunc HandlerFunc) {
+
+	r.handle(name, method, handlerFunc)
 }
-func (r *routerGroup) Post(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodPost, handleFunc)
+
+func (r *routerGroup) Get(name string, handlerFunc HandlerFunc) {
+	r.handle(name, http.MethodGet, handlerFunc)
 }
-func (r *routerGroup) Patch(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodPatch, handleFunc)
+func (r *routerGroup) Post(name string, handlerFunc HandlerFunc) {
+	r.handle(name, http.MethodPost, handlerFunc)
 }
-func (r *routerGroup) Delete(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodDelete, handleFunc)
+func (r *routerGroup) Patch(name string, handlerFunc HandlerFunc) {
+	r.handle(name, http.MethodPatch, handlerFunc)
 }
-func (r *routerGroup) Put(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodPut, handleFunc)
+func (r *routerGroup) Delete(name string, handlerFunc HandlerFunc) {
+	r.handle(name, http.MethodDelete, handlerFunc)
 }
-func (r *routerGroup) Options(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodOptions, handleFunc)
+func (r *routerGroup) Put(name string, handlerFunc HandlerFunc) {
+	r.handle(name, http.MethodPut, handlerFunc)
 }
-func (r *routerGroup) Head(name string, handleFunc HandleFunc) {
-	r.handle(name, http.MethodHead, handleFunc)
+func (r *routerGroup) Options(name string, handlerFunc HandlerFunc) {
+	r.handle(name, http.MethodOptions, handlerFunc)
+}
+func (r *routerGroup) Head(name string, handlerFunc HandlerFunc) {
+	r.handle(name, http.MethodHead, handlerFunc)
 }
 
 type routerGroup struct {
 	groupName        string
-	handlerMap       map[string]map[string]HandleFunc
+	handlerMap       map[string]map[string]HandlerFunc
 	handlerMethodMap map[string][]string
+	treeNode         *treeNode
 }
 
 type Engine struct {
@@ -82,37 +89,34 @@ func New() *Engine {
 }
 
 func (e *Engine) ServeHTTP(w http.ResponseWriter, r *http.Request) {
-	groups := e.router.groups
-	for _, g := range groups {
-		for name, methodHandle := range g.handlerMap {
-			url := "/" + g.groupName + name
-			if r.RequestURI == url {
-				ctx := &Context{
-					W: w,
-					R: r,
-				}
-				_, ok := methodHandle["ANY"]
-				if ok {
-					methodHandle["ANY"](ctx)
-					return
-				}
-				method := r.Method
-				handler, ok := methodHandle[method]
-				if ok {
-					handler(ctx)
-					return
-				}
-				w.WriteHeader(http.StatusMethodNotAllowed)
-				fmt.Fprintln(w, method+" not allowed ")
-				return
-			} else {
-				w.WriteHeader(http.StatusNotFound)
-				fmt.Fprintln(w, r.RequestURI+" not found ")
+
+	for _, g := range e.routerGroups {
+		routerName := SubStringLast(r.RequestURI, "/"+g.groupName)
+		node := g.treeNode.Get(routerName)
+		if node != nil && node.isEnd {
+			ctx := &Context{
+				W: w,
+				R: r,
+			}
+			handle, ok := g.handlerMap[node.routerName]["ANY"]
+			if ok {
+				handle(ctx)
 				return
 			}
+			method := r.Method
+			handler, ok := g.handlerMap[node.routerName][method]
+			if ok {
+				handler(ctx)
+				return
+			}
+			w.WriteHeader(http.StatusMethodNotAllowed)
+			fmt.Fprintln(w, method+" not allowed ")
+			return
 		}
+		w.WriteHeader(http.StatusNotFound)
+		fmt.Fprintln(w, r.RequestURI+" not found ")
+		return
 	}
-
 }
 
 func (e *Engine) Run() {
